3c72c5f9d195fe06666ccae6facd5f207c70c07b
Arnas Danaitis <arnas.danaitis@gmail.com>
2019-03-02 15:44:19 +0200

    Cygwin compilation script to ease things

---
+++ cygwin-compile.bat
@@ -0,0 +1,22 @@
+@echo off
+if not exist bin\cygwin1.dll (
+    echo Cygwin dll missing, copying
+    xcopy C:\cygwin64\bin\cygwin1.dll bin\
+)
+
+set compiledir=C:\cygwin64\home\compilation\
+
+mkdir %compiledir%
+echo cd /home/compilation/ > %compiledir%\script.sh
+echo mkdir bin >> %compiledir%\script.sh
+
+for %%f in (*.c) do (
+    xcopy /y %%f %compiledir%
+    echo gcc %%f -o bin/%%~nf.exe >> %compiledir%\script.sh
+)
+
+ C:\cygwin64\bin\bash --login -i -c "dos2unix /home/compilation/script.sh && sh /home/compilation/script.sh"
+
+ move %compiledir%\bin\* bin
+
+ rmdir /s /q %compiledir%
